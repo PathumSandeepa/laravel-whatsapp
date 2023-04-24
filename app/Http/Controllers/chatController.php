54048b858355589e407181e6acb85acd4c0bef6a
Pathum Sandeepa <pathumsandeepa13@gmail.com>
2023-04-24 11:07:30 +0530

    change some relations and comments

--- app/Http/Controllers/chatController.php
+++ app/Http/Controllers/chatController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\TblList;
 use App\Models\User;
+// use Twilio\Rest\Client;
 // use App\Models\TblList;
 // use App\Models\Tpno;
 
 use Illuminate\Http\Request;
+use Spatie\FlareClient\Http\Client as HttpClient;
 
 class chatController extends Controller
 {
     public function chatInterface()
     {
         $lists = TblList::with('tableListRelation')->get();
-        dd($lists);
         return view('chat', ['lists' => $lists]);
-
     }
+
+
 }
